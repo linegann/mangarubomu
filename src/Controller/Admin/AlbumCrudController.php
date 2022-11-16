5bd3b9a6ca54e46e2963d913a86aafde381fed5c
linegann <bilal.boumeslout@telecom-sudparis.eu>
2022-11-16 16:50:24 +0100

    Upgraded Admin to show Characters in Album Section

--- src/Controller/Admin/AlbumCrudController.php
+++ src/Controller/Admin/AlbumCrudController.php
@@ -8,6 +8,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 
 
 class AlbumCrudController extends AbstractCrudController
@@ -24,7 +26,16 @@ class AlbumCrudController extends AbstractCrudController
 
             AssociationField::new('characters')
                 ->autocomplete()
-                ->setFormTypeOption('by_reference', false),
+                ->setFormTypeOption('by_reference', false)
+                ->setTemplatePath('admin/album/characters.html.twig'),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+
+    return $actions
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+    ;
     }
 }
